bcb5b8fcf8e125ea8162db31b5a6c768a7272f40
jwtlt <sinanbozkurt37@gmail.com>
2022-09-30 19:29:37 +0300

    configuration.properties file added

--- src/test/java/trendyol/pages/Base_Page.java
+++ src/test/java/trendyol/pages/Base_Page.java
@@ -1,4 +1,34 @@
 package trendyol.pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+import trendyol.utilities.*;
+
+import static trendyol.utilities.ConfigurationReader.*;
+
 public class Base_Page {
+
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(),5);
+
+    @FindBy(xpath = "//div/p[text()='Giriş Yap']")
+    public WebElement loginButton;
+
+    @FindBy(id = "#login-email")
+    public WebElement usernameBox;
+
+    @FindBy(id = "#login-password-input")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement submitButton;
+
+    public void login(){
+        loginButton.click();
+        wait.until(ExpectedConditions.visibilityOf(submitButton));
+        usernameBox.sendKeys(getProperty("username"));
+        passwordBox.sendKeys(getProperty("password"));
+        submitButton.click();
+    }
+
 }
